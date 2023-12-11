b1434590d20ed6abc937c397e2cbe1268bde9132
ingoboerner <ingoboerner@gmx.at>
2023-12-11 09:13:41 +0100

    added clscore module stub

---
+++ horace/clscor.py
@@ -0,0 +1,290 @@
+"""
+CLSCor
+Additional functionality for CLS INFRA/CLSCor integration of POSTDATA Resources
+"""
+
+from rdflib import Graph, URIRef, RDF, RDFS, Literal
+import hashlib
+from core import CRM, LRM, CORE, CLS, DIG
+
+def shorthash(textstring: str, chars: int = 10) -> str:
+    """Create a trunctaded sha256 hash of a string
+
+    Args:
+        textstring: Text to produce a shorthash from.
+        chars (int): length of shorted hash. Defaults to 10.
+
+    Returns:
+        str: shorted sha256 hash
+    """
+    hashed = hashlib.sha256(textstring.encode("UTF-8")).hexdigest()
+    shorthash = hashed[:chars]
+    return shorthash
+
+
+def generate_uri(identifier: str, base:str = "https://clscor.io/entity", type:str = None) -> str:
+    """Generate a URI. Will hash the idententifier to desematisize
+    """
+    hash = shorthash(identifier)
+    if type is not None:
+        uri = f"{base}/{type}/{hash}"
+    else:
+        uri = f"{base}/{hash}"
+    return uri
+
+
+# Corpora that can be downloaded with averell are here: (IB)
+    # https://github.com/linhd-postdata/averell-docker/blob/main/src/averell/corpora.yaml
+
+    # disco2_1: Disco V2.1, https://github.com/pruizf/disco/archive/v2.1.zip
+    # disco3: Disco V3, https://github.com/pruizf/disco/archive/v3.zip
+    # adso: Sonetos Siglo de Oro, https://github.com/bncolorado/CorpusSonetosSigloDeOro/archive/master.zip
+    # adso 100: ADSO 100 poems corpus, https://github.com/linhd-postdata/adsoScansionSystem/releases/download/1.0.0/ADSO_gold_standard_100poems.zip
+    # plc: Poesía Lírica Castellana Siglo de Oro, https://github.com/bncolorado/CorpusGeneralPoesiaLiricaCastellanaDelSigloDeOro/archive/master.zip
+    # gongo: Gongocorpus, https://github.com/linhd-postdata/gongocorpus/archive/master.zip
+    # fbfv: [This is not in the averell list!]
+    # ecpa: Eighteenth Century Poetry Archive, https://github.com/alhuber1502/ECPA/archive/master.zip
+
+    # Don't know what fbfv is, need to check if it gets loaded at all... (IB)
+
+# took the metadata from the averell repo and included it here (IB)
+
+corpus_metadata = {
+  "disco2_1": {
+    "name": "Disco V2.1",
+    "properties": {
+      "slug": "disco2_1",
+      "license": "CC-BY",
+      "language": "es",
+      "reader": "averell.readers.disco",
+      "url": "https://github.com/pruizf/disco/archive/v2.1.zip",
+      "size": "22M",
+      "doc_quantity": 4088,
+      "word_quantity": 381539,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "disco3": {
+    "name": "Disco V3",
+    "properties": {
+      "slug": "disco3",
+      "license": "CC-BY",
+      "language": "es",
+      "reader": "averell.readers.disco3",
+      "url": "https://github.com/pruizf/disco/archive/v3.zip",
+      "size": "28M",
+      "doc_quantity": 4080,
+      "word_quantity": 377978,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "adso": {
+    "name": "Sonetos Siglo de Oro",
+    "properties": {
+      "slug": "adso",
+      "license": "CC-BY-NC 4.0",
+      "language": "es",
+      "reader": "averell.readers.sdo",
+      "url": "https://github.com/bncolorado/CorpusSonetosSigloDeOro/archive/master.zip",
+      "size": "6.8M",
+      "doc_quantity": 5078,
+      "word_quantity": 466012,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "adso100": {
+    "name": "ADSO 100 poems corpus",
+    "properties": {
+      "slug": "adso100",
+      "license": "CC-BY-NC 4.0",
+      "language": "es",
+      "reader": "averell.readers.sdo",
+      "url": "https://github.com/linhd-postdata/adsoScansionSystem/releases/download/1.0.0/ADSO_gold_standard_100poems.zip",
+      "size": "128K",
+      "doc_quantity": 100,
+      "word_quantity": 9208,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "plc": {
+    "name": "Poesía Lírica Castellana Siglo de Oro",
+    "properties": {
+      "slug": "plc",
+      "license": "CC-BY-NC 4.0",
+      "language": "es",
+      "reader": "averell.readers.plsdo",
+      "url": "https://github.com/bncolorado/CorpusGeneralPoesiaLiricaCastellanaDelSigloDeOro/archive/master.zip",
+      "size": "3.8M",
+      "doc_quantity": 475,
+      "word_quantity": 299402,
+      "granularity": [
+        "stanza",
+        "line",
+        "word",
+        "syllable"
+      ]
+    }
+  },
+  "gongo": {
+    "name": "Gongocorpus",
+    "properties": {
+      "slug": "gongo",
+      "license": "CC-BY-NC-ND 3.0 FR",
+      "language": "es",
+      "reader": "averell.readers.gongocorpus",
+      "url": "https://github.com/linhd-postdata/gongocorpus/archive/master.zip",
+      "size": "9.2M",
+      "doc_quantity": 481,
+      "word_quantity": 99079,
+      "granularity": [
+        "stanza",
+        "line",
+        "word",
+        "syllable"
+      ]
+    }
+  },
+  "ecpa": {
+    "name": "Eighteenth Century Poetry Archive",
+    "properties": {
+      "slug": "ecpa",
+      "license": "CC BY-SA 4.0",
+      "language": "en",
+      "reader": "averell.readers.ecpa",
+      "url": "https://github.com/alhuber1502/ECPA/archive/master.zip",
+      "size": "2400M",
+      "doc_quantity": 3084,
+      "word_quantity": 2063668,
+      "granularity": [
+        "stanza",
+        "line",
+        "word"
+      ]
+    }
+  },
+  "4b4v": {
+    "name": "For Better For Verse",
+    "properties": {
+      "slug": "4b4v",
+      "license": "Unknown",
+      "language": "en",
+      "reader": "averell.readers.forbetter4verse",
+      "url": "https://github.com/waynegraham/for_better_for_verse/archive/master.zip",
+      "size": "39.5M",
+      "doc_quantity": 103,
+      "word_quantity": 41749,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "mel": {
+    "name": "Métrique en Ligne",
+    "properties": {
+      "slug": "mel",
+      "license": "Unknown",
+      "language": "fr",
+      "reader": "averell.readers.metriqueenligne",
+      "url": "https://github.com/linhd-postdata/metrique-en-ligne/archive/master.zip",
+      "size": "183M",
+      "doc_quantity": 5081,
+      "word_quantity": 1850222,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  },
+  "bibit": {
+    "name": "Biblioteca Italiana",
+    "properties": {
+      "slug": "bibit",
+      "license": "Unknown",
+      "language": "it",
+      "reader": "averell.readers.bibliotecaitaliana",
+      "url": "https://github.com/linhd-postdata/biblioteca_italiana/archive/master.zip",
+      "size": "242M",
+      "doc_quantity": 25341,
+      "word_quantity": 7121246,
+      "granularity": [
+        "stanza",
+        "line",
+        "word"
+      ]
+    }
+  },
+  "czverse": {
+    "name": "Corpus of Czech Verse",
+    "properties": {
+      "slug": "czverse",
+      "license": "CC-BY-SA",
+      "language": "cs",
+      "reader": "averell.readers.czechverse",
+      "url": "https://github.com/versotym/corpusCzechVerse/archive/refs/heads/master.zip",
+      "size": "4100M",
+      "doc_quantity": 66428,
+      "word_quantity": 12636867,
+      "granularity": [
+        "stanza",
+        "line",
+        "word"
+      ]
+    }
+  },
+  "stichopt": {
+    "name": "Stichotheque Portuguese",
+    "properties": {
+      "slug": "stichopt",
+      "license": "Unkwown",
+      "language": "pt",
+      "reader": "averell.readers.stichotheque",
+      "url": "https://gitlab.com/stichotheque/stichotheque-pt/-/archive/master/stichotheque-pt-master.zip",
+      "size": "11.8M",
+      "doc_quantity": 1702,
+      "word_quantity": 168411,
+      "granularity": [
+        "stanza",
+        "line"
+      ]
+    }
+  }
+}
+
+
+def generate_corpus_rdf(corpus_id, rdf_root: str = None):
+    """Generate RDF data for a single corpus identified by its corpus ID; function returns the URI of the corpus
+    The function will be called by the generate function in the main module to provide a means of creating a
+    corpus rdf file when the data is transformed to rdf
+    rdf_root is passed by the generate function
+    """
+    # get corpus metadata
+    cdata = corpus_metadata[corpus_id]
+    g = Graph()
+    g.bind("crm", CRM)
+    g.bind("lrm", LRM)
+    g.bind("pdc", CORE)
+    g.bind("dig", DIG)
+    g.bind("cls", CLS)
+    corpus_uri = generate_uri(f"postdata/corpus/{corpus_id}")
+    corpus = URIRef(corpus_uri)
+    g.add((corpus, RDF.type, CLS.X1_Corpus))
+    g.add((corpus, RDFS.label, Literal(f"{cdata['name']} [Corpus]")))
+
+    # need to decide where to store this
+    file_path = str(rdf_root) + "corpus_" + corpus_id.replace(".", "-") + ".ttl")
+    g.serialize(format="ttl", destination=file_path, encoding="utf-8")
+
+    return corpus_uri
